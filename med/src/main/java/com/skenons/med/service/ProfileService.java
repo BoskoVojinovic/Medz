df08ea023688ac03027001ec447d893dbe1a3e25
JelenaMatkovic <matkovic.jelena@uns.ac.rs>
2020-02-02 23:37:47 +0100

    Test

--- med/src/main/java/com/skenons/med/service/ProfileService.java
+++ med/src/main/java/com/skenons/med/service/ProfileService.java
@@ -1,8 +1,12 @@
 package com.skenons.med.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skenons.med.data.Profile;
 import com.skenons.med.repo.IProfileRepo;
 
 @Service
@@ -10,7 +14,12 @@ public class ProfileService
 {
 	@Autowired
 	IProfileRepo clinics;
-	
+	@Autowired
+	private IProfileRepo profileRepo;
 
 	
+
+	public List<Profile> getAll(){ 
+		return profileRepo.findAll().stream().collect(Collectors.toList());
+	};
 }
