11d188b5afd1afdc5b95e6f877d71002f20caa70
SkenonS <skenons.mail@gmail.com>
2020-02-03 07:52:56 +0100

    *Front end naive attempt

--- med/src/main/java/com/skenons/med/service/ProfileService.java
+++ med/src/main/java/com/skenons/med/service/ProfileService.java
@@ -1,7 +1,6 @@
 package com.skenons.med.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,13 +12,12 @@ import com.skenons.med.repo.IProfileRepo;
 public class ProfileService
 {
 	@Autowired
-	IProfileRepo clinics;
-	@Autowired
 	private IProfileRepo profileRepo;
 
 	
 
 	public List<Profile> getAll(){ 
-		return profileRepo.findAll().stream().collect(Collectors.toList());
+		//return profileRepo.findAll().stream().collect(Collectors.toList());??
+		return profileRepo.findAll();
 	};
 }
